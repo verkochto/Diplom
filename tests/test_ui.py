b82a1967a15061458a909f2e0f0412b69d8a6ad3
verkochto <167893047+verkochto@users.noreply.github.com>
2025-06-05 17:08:37 +0300

    Create test_ui.py

---
+++ tests/test_ui.py
@@ -0,0 +1,12 @@
+import pytest
+import allure
+from pages.login_page import LoginPage
+from config import config
+
+@pytest.mark.ui
+@allure.title("UI: Успешная авторизация")
+def test_login_success(driver):
+    page = LoginPage(driver)
+    page.open()
+    page.login(config.USERNAME, config.PASSWORD)
+    assert page.user_icon_visible()
