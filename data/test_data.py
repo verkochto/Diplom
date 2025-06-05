35ca16a7bd54471f1d2312e6869409a773fbca1a
verkochto <167893047+verkochto@users.noreply.github.com>
2025-06-05 17:05:27 +0300

    Create test_data.py

---
+++ data/test_data.py
@@ -0,0 +1,9 @@
+valid_user = {
+    "email": "valid_user@example.com",
+    "password": "valid_pass"
+}
+
+invalid_user = {
+    "email": "invalid_user@example.com",
+    "password": "wrong_pass"
+}
