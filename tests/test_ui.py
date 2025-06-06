64031e6c18ac1e27376cd9bd9c8f5113523e756b
verkochto <167893047+verkochto@users.noreply.github.com>
2025-06-06 20:59:14 +0300

    Update test_ui.py

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,13 +1,58 @@
 import pytest
 import allure
-from pages.login_page import LoginPage
-from config import config
+from pages.main_page import MainPage
 
 
-@pytest.mark.ui
-@allure.title("UI: Успешная авторизация")
-def test_login_success(driver):
-    page = LoginPage(driver)
-    page.open()
-    page.login(config.USERNAME, config.PASSWORD)
-    assert page.user_icon_visible()
+@allure.epic("UI Проверка поиска")
+@allure.feature("Функциональность строкового поиска")
+class TestSearchFunctionality:
+
+    @allure.title("Поиск по ключевому слову: заголовок книги")
+    @allure.description("Убедиться, что поиск по слову 'капитанская' возвращает релевантные результаты.")
+    @pytest.mark.ui
+    def test_search_by_title_keyword(self, driver):
+        page = MainPage(driver)
+        page.open()
+        page.search_book("капитанская")
+        results = page.get_search_results_text()
+        assert "запросу «капитанская», найдено" in results
+
+    @allure.title("Поиск по фамилии писателя")
+    @allure.description("Проверка поиска по имени автора: пушкин.")
+    @pytest.mark.ui
+    def test_search_by_author_name(self, driver):
+        page = MainPage(driver)
+        page.open()
+        page.search_book("пушкин")
+        results = page.get_search_results_text()
+        assert "запросу «пушкин», найдено" in results
+
+    @allure.title("Поиск по английскому названию")
+    @allure.description("Тестирование поиска по англоязычному слову 'house'.")
+    @pytest.mark.ui
+    def test_search_english_term(self, driver):
+        page = MainPage(driver)
+        page.open()
+        page.search_book("house")
+        results = page.get_search_results_text()
+        assert "запросу «house», найдено" in results
+
+    @allure.title("Поиск по спецсимволам")
+    @allure.description("Проверка реакции на ввод несуществующего слова из символов.")
+    @pytest.mark.ui
+    def test_search_with_symbols(self, driver):
+        page = MainPage(driver)
+        page.open()
+        page.search_book("@!?*")
+        results = page.get_search_results_text()
+        assert "не принёс результатов" in results
+
+    @allure.title("Поиск по тексту на тайском языке")
+    @allure.description("Убедиться, что ввод текста на тайском не даёт результатов.")
+    @pytest.mark.ui
+    def test_search_with_thai_language(self, driver):
+        page = MainPage(driver)
+        page.open()
+        page.search_book("เกาะมหาสมบัต")
+        results = page.get_search_results_text()
+        assert "не принёс результатов" in results
